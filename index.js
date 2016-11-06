6a0049cc260e9013ac0e37a6d533d76edc39ff37
Titus Wormer <tituswormer@gmail.com>
2016-11-06 17:05:37 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module rehype:lint:util:script-supporting
- */
-
 'use strict';
 
-/* eslint-env commonjs */
+/* Expose. */
+module.exports = scriptSupporting;
 
-/*
- * Dependencies.
- */
-
+/* Dependencies. */
 var is = require('hast-util-is-element');
 
-/*
- * Tag-names.
- */
-
+/* Tag-names. */
 var names = [
-    'script',
-    'template'
+  'script',
+  'template'
 ];
 
-/**
- * Check if a node is a script-supporting element
- *
- * @param {*} node - Thing to check.
- * @return {boolean} - Whether a node is script-supporting.
- */
+/* Check if a node is a script-supporting element */
 function scriptSupporting(node) {
-    return is(node, names);
+  return is(node, names);
 }
-
-/*
- * Expose.
- */
-
-module.exports = scriptSupporting;
--- test.js
+++ test.js
@@ -1,84 +1,55 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module hast:util:script-supporting
- * @fileoverview Test suite for `script-supporting`.
- */
-
 'use strict';
 
-/* eslint-env node */
-
-/*
- * Module dependencies.
- */
-
+/* Dependencies. */
 var test = require('tape');
 var scriptSupporting = require('./index.js');
 
-/*
- * Tests.
- */
-
+/* Tests. */
 test('scriptSupporting', function (t) {
-    t.equal(
-        scriptSupporting(),
-        false,
-        'should return `false` without node'
-    );
+  t.equal(
+    scriptSupporting(),
+    false,
+    'should return `false` without node'
+  );
 
-    t.equal(
-        scriptSupporting(null),
-        false,
-        'should return `false` with `null`'
-    );
+  t.equal(
+    scriptSupporting(null),
+    false,
+    'should return `false` with `null`'
+  );
 
-    t.equal(
-        scriptSupporting({
-            'type': 'text'
-        }),
-        false,
-        'should return `false` when without `element`'
-    );
+  t.equal(
+    scriptSupporting({type: 'text'}),
+    false,
+    'should return `false` when without `element`'
+  );
 
-    t.equal(
-        scriptSupporting({
-            'type': 'element'
-        }),
-        false,
-        'should return `false` when with invalid `element`'
-    );
+  t.equal(
+    scriptSupporting({type: 'element'}),
+    false,
+    'should return `false` when with invalid `element`'
+  );
 
-    t.equal(
-        scriptSupporting({
-            'type': 'element',
-            'tagName': 'a',
-            'properties': {
-                'href': '#alpha',
-                'title': 'Bravo'
-            },
-            'children': [{
-                'type': 'text',
-                'value': 'Charlie'
-            }]
-        }),
-        false,
-        'should return `false` when without not script-supporting'
-    );
+  t.equal(
+    scriptSupporting({
+      type: 'element',
+      tagName: 'a',
+      properties: {href: '#alpha', title: 'Bravo'},
+      children: [{type: 'text', value: 'Charlie'}]
+    }),
+    false,
+    'should return `false` when without not script-supporting'
+  );
 
-    t.equal(
-        scriptSupporting({
-            'type': 'element',
-            'tagName': 'template',
-            'children': [{
-                'type': 'text',
-                'value': 'Delta'
-            }]
-        }),
-        true,
-        'should return `true` when with script-supporting'
-    );
+  t.equal(
+    scriptSupporting({
+      type: 'element',
+      tagName: 'template',
+      children: [{type: 'text', value: 'Delta'}]
+    }),
+    true,
+    'should return `true` when with script-supporting'
+  );
 
-    t.end();
+  t.end();
 });
