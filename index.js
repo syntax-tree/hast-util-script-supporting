ce84ed53a38a2c2cc438167aabc04bb6f50fd206
Titus Wormer <tituswormer@gmail.com>
2021-04-25 19:41:06 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+/**
+ * @typedef {import('hast').Element & {tagName: 'script'|'template'}} ScriptOrTemplate
+ * @typedef {import('hast-util-is-element').AssertPredicate<ScriptOrTemplate>} AssertScriptOrTemplate
+ */
+
 import {convertElement} from 'hast-util-is-element'
 
-/* Check if a node is a script-supporting element */
+/**
+ * Check if a node is a script-supporting element.
+ * @type {AssertScriptOrTemplate}
+ */
+// @ts-ignore Sure, the assertion matches.
 export const scriptSupporting = convertElement(['script', 'template'])
