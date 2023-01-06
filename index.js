2208b4b96914017bb9fafbb4bc6644c9f9452a53
Titus Wormer <tituswormer@gmail.com>
2023-01-06 14:29:41 +0100

    Refactor code-style
    *   Add more docs to JSDoc

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 /**
- * @typedef {import('hast').Element & {tagName: 'script'|'template'}} ScriptOrTemplate
- * @typedef {import('hast-util-is-element').AssertPredicate<ScriptOrTemplate>} AssertScriptOrTemplate
+ * @typedef {import('hast').Element} Element
+ */
+
+/**
+ * @typedef {import('hast-util-is-element').AssertPredicate<Element & {tagName: 'script' | 'template'}>} AssertScriptOrTemplate
  */
 
 import {convertElement} from 'hast-util-is-element'
 
 /**
  * Check if a node is a script-supporting element.
+ *
  * @type {AssertScriptOrTemplate}
+ * @param value
+ *   Thing to check (typically `Node`).
+ * @returns
+ *   Whether `value` is a script-supporting element.
  */
 // @ts-expect-error Sure, the assertion matches.
 export const scriptSupporting = convertElement(['script', 'template'])
