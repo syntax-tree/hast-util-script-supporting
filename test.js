8fdb90c920370f2139241ef12ad5a27e9a707cec
Titus Wormer <tituswormer@gmail.com>
2023-08-01 11:52:26 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,53 +1,63 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {scriptSupporting} from './index.js'
-import * as mod from './index.js'
 
-test('scriptSupporting', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['scriptSupporting'],
-    'should expose the public api'
+test('scriptSupporting', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'scriptSupporting'
+    ])
+  })
+
+  await t.test('should return `false` without node', async function () {
+    // @ts-expect-error `node` should be given.
+    assert.equal(scriptSupporting(), false)
+  })
+
+  await t.test('should return `false` with `null`', async function () {
+    assert.equal(scriptSupporting(null), false)
+  })
+
+  await t.test(
+    'should return `false` when without `element`',
+    async function () {
+      assert.equal(scriptSupporting({type: 'text'}), false)
+    }
   )
 
-  assert.equal(scriptSupporting(), false, 'should return `false` without node')
-
-  assert.equal(
-    scriptSupporting(null),
-    false,
-    'should return `false` with `null`'
+  await t.test(
+    'should return `false` when with invalid `element`',
+    async function () {
+      assert.equal(scriptSupporting({type: 'element'}), false)
+    }
   )
 
-  assert.equal(
-    scriptSupporting({type: 'text'}),
-    false,
-    'should return `false` when without `element`'
+  await t.test(
+    'should return `false` when without not script-supporting',
+    async function () {
+      assert.equal(
+        scriptSupporting({
+          type: 'element',
+          tagName: 'a',
+          properties: {href: '#alpha', title: 'Bravo'},
+          children: [{type: 'text', value: 'Charlie'}]
+        }),
+        false
+      )
+    }
   )
 
-  assert.equal(
-    scriptSupporting({type: 'element'}),
-    false,
-    'should return `false` when with invalid `element`'
-  )
-
-  assert.equal(
-    scriptSupporting({
-      type: 'element',
-      tagName: 'a',
-      properties: {href: '#alpha', title: 'Bravo'},
-      children: [{type: 'text', value: 'Charlie'}]
-    }),
-    false,
-    'should return `false` when without not script-supporting'
-  )
-
-  assert.equal(
-    scriptSupporting({
-      type: 'element',
-      tagName: 'template',
-      children: [{type: 'text', value: 'Delta'}]
-    }),
-    true,
-    'should return `true` when with script-supporting'
+  await t.test(
+    'should return `true` when with script-supporting',
+    async function () {
+      assert.equal(
+        scriptSupporting({
+          type: 'element',
+          tagName: 'template',
+          children: [{type: 'text', value: 'Delta'}]
+        }),
+        true
+      )
+    }
   )
 })
